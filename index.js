3c4c611eb82dd81a9479182229dd5419ac48433d
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-27 04:12:21 -0500

    first commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * pkg-homepage <https://github.com/jonschlinkert/pkg-homepage>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
