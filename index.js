e9b182f2c594d1891b047b079b2e6789ac53ea61
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-20 09:40:54 -0500

    get homepage

--- index.js
+++ index.js
@@ -7,6 +7,18 @@
 
 'use strict';
 
-module.exports = function () {
-  // do stuff
+var utils = require('./utils');
+
+module.exports = function(pkg) {
+  if (!pkg) return null;
+  if (pkg.homepage) return pkg.homepage;
+  if (!pkg.repository) return null;
+
+  var res = {};
+  if (typeof pkg.repository === 'string') {
+    res = utils.parse(pkg.repository);
+  } else if (typeof pkg.repository === 'object') {
+    res = utils.parse(pkg.repository.url);
+  }
+  return utils.stringify(res.user, res.repo);
 };
